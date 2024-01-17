52d5df5335f57e06dc8f51e2cbb8752136e7015e
Anjalivibrant <anjali90wr@gmail.com>
2024-01-17 09:28:18 +0530

    changes made in the quiz-world file

---
+++ index1.js
@@ -0,0 +1,455 @@
+const questions = [
+    {
+        question: "What is the collection of the hyperlinked document on the internet known as?",
+        optionA: "HTML",
+        optionB: "Email",
+        optionC: "WWW",
+        optionD: "Internet",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "The arrangement where all data pass through a central computer is known as",
+        optionA: "Star topology",
+        optionB: "Mesh topology",
+        optionC: "Ring topology",
+        optionD: "Bus topology",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Choose among the following which is a bit-oriented protocol.",
+        optionA: "SSL",
+        optionB: "HDLC",
+        optionC: "HTTP",
+        optionD: "All of the above",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Identify the device used to boost up a weak signal.",
+        optionA: "Modem",
+        optionB: "Repeater",
+        optionC: "Switch",
+        optionD: "Router",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Which of these was the first antivirus software ever created?",
+        optionA: "Reaper",
+        optionB: "Ray Tomlinson",
+        optionC: "Creeper",
+        optionD: "Tinkered",
+        correctOption: "optionA"
+    },
+
+    {
+        question:"__________ is an application which now comes built-in Windows OS & it allows Windows users to encrypt all drive for security purpose.",
+        optionA: "MS Windows Defender",
+        optionB: "MSE",
+        optionC: "BitLocker",
+        optionD: "MS Office",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "___________ are essential because they frequently comprises of critical patches to security holes.",
+        optionA: "System Software",
+        optionB: "Utility Software",
+        optionC: "Software Executables",
+        optionD: "Softwre Updates",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "8.	If p is an integer pointer with a value 1000, then what will the value of p + 5 be?",
+        optionA: "1020",
+        optionB: "1005",
+        optionC: "1004",
+        optionD: "1010",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Which of the following is a commonly used programming language for scientific computing?",
+        optionA: "Java",
+        optionB: "Python",
+        optionC: "Ruby",
+        optionD: "C#",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Bipolar encoding scheme represents how many voltage levels?",
+        optionA: "2",
+        optionB: "3",
+        optionC: "4",
+        optionD: "5",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "What does “HTTP” signify on a website’s URL?",
+        optionA: "Only sent data is encrypted. ",
+        optionB: "Only received data is encrypted. ",
+        optionC: "Data being sent and received is encrypted. ",
+        optionD: "Data isn’t encrypted but is hidden from the hackers. ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which of the following won’t risk a malware attack on your system?",
+        optionA: "Illegal downloads from torrents.",
+        optionB: "Using free operating system like Linux instead of Windows.",
+        optionC: "Not checking for email attachment authenticity.",
+        optionD: "Not updating applications and devices.",
+        correctOption: "optionB"
+    },
+
+
+    {
+        question: "Which attack will keep track of all keyboard inputs on an infected system?",
+        optionA: "Key jacking ",
+        optionB: "Keylogger",
+        optionC: "Keyboard Monitor ",
+        optionD: "All of the above",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Which two aspects of data do we need to control in order to maintain data integrity?",
+        optionA: "Access control, file permission ",
+        optionB: "Network, file permission ",
+        optionC: "Access control, file detection ",
+        optionD: "Arithmetic operator",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "What are the two types of firewalls?",
+        optionA: "Home based and internet based ",
+        optionB: "Free and Premium ",
+        optionC: "Digital and Electronic ",
+        optionD: "Hardware and Software ",
+        correctOption: "optionD"
+    },
+
+    {
+        question: " How long is an IPv6 address? ",
+        optionA: "32 bits ",
+        optionB: "64 bits ",
+        optionC: "128 bits ",
+        optionD: "256 bits ",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "What is the primary function of a router in a network? ",
+        optionA: "Forwarding data between different networks. ",
+        optionB: "Connecting devices within the same LAN. ",
+        optionC: "Forwarding data between devices on the same network. ",
+        optionD: "Managing network security protocols.",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "What is the function of ICMP (Internet Control Message Protocol)?",
+        optionA: "To encapsulate data for transmission",
+        optionB: "To establish secure tunnels. ",
+        optionC: "To manage network secure tunnels.",
+        optionD: "To diagnose network connectivity issues.",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which protocol is used for securely accessing a remote server over an unsecured network?",
+        optionA: "FTP",
+        optionB: "SSH",
+        optionC: "HTTP",
+        optionD: "SNMP",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "What is the purpose of VLANs (Virtual Local Area Network)? ",
+        optionA: "Logical segmentation of a network. ",
+        optionB: "Load balancing. ",
+        optionC: "Dynamic IP address assignment. ",
+        optionD: "Encryption of data packets. ",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Firewall examines each ____________ that are entering or leaving the internal network.",
+        optionA: "Emails users",
+        optionB: "Updates",
+        optionC: "Connections",
+        optionD: "Data packets",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "What is the output of the following code? int main() {for(i=0;i<10;i++); printf('%d',i); return 0;}",
+        optionA: "10",
+        optionB: "0123456789",
+        optionC: "syntax error",
+        optionD: "Infinite loop",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Which of the following data is not appropriate here, if you delete cache, cookies, and browser history?",
+        optionA: "Address bar predictions",
+        optionB: "Saved passwords",
+        optionC: "Browser plug-ins",
+        optionD: "Shopping cart content",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "It can be a software program or a hardware device that filters all data packets coming through the internet,a network etc.it is known as the_______",
+        optionA: "Antivirus",
+        optionB: "Firewall",
+        optionC: "Cookies",
+        optionD: "Malware",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Which of the following explains cookies nature?",
+        optionA: "Non Volatile",
+        optionB: "Volatile",
+        optionC: "Intrasient",
+        optionD: "Trasient",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Which of the following is the technique that is used to verify messages integrity?",
+        optionA: "Message digest",
+        optionB: "Protocol",
+        optionC: "Decryption algorithm",
+        optionD: "Digital Signature",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "The code red is a sort of a___________",
+        optionA: "Video Editiing Software",
+        optionB: "Computer Virus",
+        optionC: "Photo editing software",
+        optionD: "Antivirus program",
+        correctOption: "optionB"
+    },
+
+    {
+        question: " The DNS would translate any domain name into_________",
+        optionA: "IP",
+        optionB: "URL",
+        optionC: "Binary",
+        optionD: "Hex",
+        correctOption: "optionB"
+    },
+    
+    {
+        question: " Which of these is usually referred to as the port number(default) of several web servers like apache?",
+        optionA: "40",
+        optionB: "20",
+        optionC: "87",
+        optionD: "80",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "  Which of these is a sort of independent type of malicious program that would not require any host program?",
+        optionA: "Virus",
+        optionB: "Trap Door",
+        optionC: "Worm",
+        optionD: "Trojan",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Which of the following statement is TRUE about SQL injection?",
+        optionA: "SQL injection is an object penetration technique",
+        optionB: "SQL injection is a function penetration technique",
+        optionC: "SQL injection is a code penetration technique",
+        optionD: "SQL injection ia s database penetration technique",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "What will be the output of the following code snippet? int main(){ int val=1; do{val++; ++val; } while(val++>25); printf('%d\n', val); return 0;}",
+        optionA: "2",
+        optionB: "8",
+        optionC: "None of these",
+        optionD: "4",
+        correctOption: "optionD"
+    },
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
+    while (shuffledQuestions.length <= 29) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++ //adds 1 to wrong attempts 
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer() //check if player picked right or wrong option
+    unCheckRadioButtons()
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
+    setTimeout(() => {
+        if (indexNumber <= 29) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 15) {
+        remark = "Good Work, Keep Practicing."
+        remarkColor = "pink"
+    }
+    else if (playerScore >= 14 && playerScore < 23) {
+        remark = "Awesome, Keep it up!"
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 27) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 30) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal, resets game and reshuffles questions
+// function closeScoreModal() {
+//     questionNumber = 1
+//     playerScore = 0
+//     wrongAttempt = 0
+//     indexNumber = 0
+//     shuffledQuestions = []
+//     NextQuestion(indexNumber)
+//     document.getElementById('score-modal').style.display = "none"
+// }
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
+
+window.onbeforeunload = function() {
+    return "Data will be lost if you leave the page, are you sure?";
+};
